7ad46433114dfcb5b27fd587159bc3ae87b6cbb4
nassima khadir <nassima.khadir@uit.ac.ma>
2023-03-09 10:49:45 +0100

    addPylint

--- test/my-module.py
+++ test/my-module.py
@@ -2,12 +2,14 @@ from requests import get
 
 
 def get_error_message(error_type):
-    colors = {
-        404: 'red',
-        403: 'orange',
-        401: 'yellow',
-    }
-    return colors[error_type] if error_type in colors else 'blue'
+    if error_type == 404:
+        return 'red'
+    elif error_type == 403:
+        return 'orange'
+    elif error_type == 401:
+        return 'yellow'
+    else:
+        return 'blue'
 
 
 def main():
@@ -21,14 +23,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-""" def get_error_message(error_type):
-    if error_type == 404:
-        return 'red'
-    elif error_type == 403:
-        return 'orange'
-    elif error_type == 401:
-        return 'yellow'
-    else:
-        return 'blue' """
